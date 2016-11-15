0bca8e25567012479e351b237a211708bf68ce07
Yaniss Amaury Chnakib <contact@yanisschnakib.fr>
2016-11-15 23:49:52 +0100

    First commit

---
+++ index.js
@@ -0,0 +1,19 @@
+module.exports =  {
+	scripts: [
+		'js/affix',
+		'js/alert',
+		'js/button',
+		'js/carrousel',
+		'js/collapse',
+		'js/dropdown',
+		'js/modal',
+		'js/popover',
+		'js/scrollspy',
+		'js/tab',
+		'js/tooltip',
+		'js/transition'
+	]
+	styles: [
+		'bootstrap/bootstrap'
+	]
+}
